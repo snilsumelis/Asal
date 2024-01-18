ef4f6d462514c7403a23e6437d5b8824ec784439
nilsu melis sönmez <80063055+snilsumelis@users.noreply.github.com>
2024-01-18 21:48:34 +0300

    update

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("1 - 100 arasındaki asal sayılar:");
+        for (int i = 2; i <= 100; i++) {
+            if (asalMi(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean asalMi(int sayi) {
+        if (sayi < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(sayi); i++) {
+            if (sayi % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
